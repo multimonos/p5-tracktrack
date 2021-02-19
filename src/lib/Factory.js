5d708df503e48cc62772fc55adc00dcf01895ac6
Craig Hopgood <searaig@gmail.com>
2021-02-19 17:42:18 -0500

    Factory and tests

---
+++ src/lib/Factory.js
@@ -0,0 +1,32 @@
+import {
+    equals,
+    identity,
+    ifElse,
+    merge,
+    pipe,
+    repeat,
+    type,
+    curry,
+} from "ramda";
+
+//fns
+const isObject = pipe( type, equals( "Object" ) )
+const throwInvalidArgumentError = () => { throw new Error( `Argument should be of type "object"` ) }
+
+
+const Factory = {
+
+    //usage: Factory.create( Model.Schema )( defaults || {} )
+    create: curry( ( schema, defaults ) =>
+        pipe(
+            ifElse( isObject, identity, throwInvalidArgumentError ),
+            merge( schema ),
+        )( defaults ) ),
+
+    //usage: Factory.createMany(5)( Model.Schema )( defaults || {} )
+    createMany: curry( ( count, schema, defaults ) =>
+        repeat( Factory.create( schema, defaults ), count )
+    )
+}
+
+export default Factory
---
+++ tests/Factory.test.js
@@ -0,0 +1,56 @@
+import Factory from "../src/lib/Factory";
+
+describe( `Factory`, () => {
+
+    describe( `.create( Model.Schema )( defaults || {} )`, () => {
+
+        it( `creates an empty object with no properties`, () => {
+            const a = Factory.create( {} )( {} )
+            expect( a ).toEqual( {} )
+        } )
+
+        it( `is curried`, () => {
+            const a = Factory.create( {}, {} )
+            expect( a ).toEqual( {} )
+        } )
+
+        it.each( [
+            [undefined],
+            [null],
+            [1],
+            ['xxx'],
+            [[]],
+            [[[]]],
+            // [{}], // should not throw
+        ] )( `throws an error if 2nd argument is '%s'`, ( val ) => {
+            expect( () => {
+                const s = Factory.create( {} )( val )
+            } ).toThrow()
+        } )
+
+    } )
+
+    describe( `.createMany( count )( Model.Schema )( defaults || {} )`, () => {
+
+        it( `returns a list`, () => {
+            const a = Factory.createMany( 3 )( {} )( {} )
+            expect( Array.isArray( a ) ).toBe( true )
+        } )
+
+        it( `returns 'count' empty objects`, () => {
+            const a = Factory.createMany( 3 )( {} )( {} )
+            expect( Array.isArray( a ) ).toBe( true )
+            expect( a ).toHaveLength( 3 )
+            expect( a ).toEqual( [{}, {}, {}] )
+        } )
+
+        it( `is curried`, () => {
+            const a = Factory.createMany( 5, {}, {} )
+            expect( Array.isArray( a ) ).toBe( true )
+            expect( a ).toHaveLength( 5 )
+            expect( a ).toEqual( [{}, {}, {}, {}, {}] )
+        } )
+
+    } )
+
+} )
